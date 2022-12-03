ceabdcd8c6ed47d826dab430e61e3193effe6cdc
Sakinder <sakinder@live.com>
2022-12-03 15:01:53 -0500

    Added simulation results folder.

---
+++ KV260/simulation_results/images/read/385_289/M385_289.m
@@ -0,0 +1,15 @@
+RGB = imread('385_289.bmp');
+HSV = rgb2hsv(RGB)
+H=HSV(:,:,1);
+S=HSV(:,:,2);
+V=HSV(:,:,3);
+ax1 = subplot(2,2,1);
+imshow(HSV)
+ax2 = subplot(2,2,2);
+imshow(H)
+
+ax3 = subplot(2,2,3);
+imshow(S)
+
+ax4 = subplot(2,2,4);
+imshow(V)
